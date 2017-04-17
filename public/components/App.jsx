e46f92d6a20c69305b70e6013c45d25be722b5be
Rp7rf <westhouse51@gmail.com>
2017-04-17 18:13:18 +0900

    learn simple things

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import update from 'immutability-helper'
-
+import AppBar from 'material-ui/AppBar'
+import DayView from './DayView'
 
 /**
  * App Container
@@ -8,15 +9,32 @@ import update from 'immutability-helper'
  */
 export default class App extends Component {
 
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: props.title
+    }
+  }
+  onClickTitle = () => {
+    this.setState(update(this.state, { title: { $set : 'changed' } }))    
+  }
   /**
    * Render
    * @return {ReactDomElement} React DOM Element
    */
   render() {
+    const weeks = ( this.props.data || [] )
 
     return (
-      <p>{ 'hello!' }</p>
+      <div>
+        <AppBar
+          title={ this.state.title }
+          onLeftIconButtonTouchTap={ this.onClickTitle }
+        />
+        <DayView foo={ 'var' } />
+        <p>{ 'hello!' }</p>
+
+      </div>
     )
   }
 
--- public/main.jsx
+++ public/main.jsx
@@ -3,16 +3,18 @@ import { render } from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import App              from './components/App.jsx'
+import AppContainer     from './components/AppContainer.jsx'
+
+import { createStore }  from 'redux' // need
+import { Provider }     from 'react-redux' // need
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin()
 
-
 render(
   <MuiThemeProvider>
-    <App />
+    <AppContainer />
   </MuiThemeProvider>,
   document.getElementById('app')
 )
